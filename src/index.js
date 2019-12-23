f7a7d3f2be2047018ba9de53d366d5af6e07d767
afeiship <1290657123@qq.com>
2019-12-23 14:08:13 +0800

    add: log with color

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 var hasModule = require('has-module');
+var ansiColor = require('ansi-colors');;
 var DEFAULT_CALLBACK = function(notInstalled) {
-  console.warn(
-    [
-      'Not installed size: ' + notInstalled.length,
-      'npm install ' + notInstalled.join(' ')
-    ].join( '\n' )
+  console.log('Not installed size: ' + notInstalled.length);
+  console.log(
+    ansiColor.green.bold(
+      [, 'npm install ' + notInstalled.join(' ')].join(
+        '\n'
+      )
+    )
   );
 };
 
